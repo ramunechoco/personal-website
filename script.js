7bd833274b6eaea0bbc0c66ab360e9e8749b49f1
ramunechoco <48040337+ramunechoco@users.noreply.github.com>
2020-08-31 19:39:51 +0700

    final

---
+++ script.js
@@ -0,0 +1,13 @@
+function openPage(pageName, elmnt, color) {
+    var i, tab, tabcontent;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tab = document.getElementsByClassName("tab");
+    for (i = 0; i < tab.length; i++) {
+      tab[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
